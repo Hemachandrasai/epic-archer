448b58e71e9de5f11217418fbb15b3c4cf4455cb
Hemachandrasai <86651007+Hemachandrasai@users.noreply.github.com>
2021-08-19 15:56:20 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,286 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+
+var engine, world;
+var canvas;
+var palyer, playerBase, playerArcher;
+var computer, computerBase, computerArcher;
+var playerArrows = [];
+var computerArrows = [];
+var playerArcherLife = 3;
+var computerArcherLife = 3;
+var isGameOver = false;
+var isGameWin = false;
+
+function preload() {
+  backgroundImg = loadImage("./assets/backimage.gif");
+  backgroundMusic = loadSound("./assets/mixkit-game-show-fun-suspense-942.wav");
+  computerSound = loadSound("./assets/mixkit-sword-slash-swoosh-1476.mp3");
+  arrowSound = loadSound("./assets/mixkit-fast-sword-whoosh-2792.wav");
+  collideSound = loadSound("./assets/mixkit-sword-strikes-armor-2765.wav");
+  
+}
+
+function setup() {
+  canvas = createCanvas(windowWidth, windowHeight);
+
+  engine = Engine.create();
+  world = engine.world;
+
+  playerBase = new PlayerBase(300, random(450, height - 300), 180, 150);
+  player = new Player(285, playerBase.body.position.y - 153, 50, 180);
+  playerArcher = new PlayerArcher(
+    340,
+    playerBase.body.position.y - 180,
+    120,
+    120
+  );
+
+  computerBase = new ComputerBase(
+    width - 300,
+    random(450, height - 300),
+    180,
+    150
+  );
+  computer = new Computer(
+    width - 280,
+    computerBase.body.position.y - 153,
+    50,
+    180
+  );
+
+  computerArcher = new ComputerArcher(
+    width - 350,
+    computerBase.body.position.y - 180,
+    120,
+    120
+  );
+  handleComputerArcher();
+}
+
+function draw() {
+  background(backgroundImg);
+  if(!backgroundMusic.isPlaying()){
+    backgroundMusic.play();
+    backgroundMusic.setVolume(0.1);
+  }
+
+  Engine.update(engine);
+
+  // Title
+  fill("#FFFF");
+  textAlign("center");
+  textSize(40);
+  text("EPIC ARCHERY", width / 2, 100);
+
+  for (var i = 0; i < playerArrows.length; i++) {
+    showArrows(i, playerArrows);
+  }
+
+  playerBase.display();
+  player.display();
+  player.life();
+  playerArcher.display();
+  handlePlayerArrowCollision();
+
+  for (var i = 0; i < computerArrows.length; i++) {
+    showArrows(i, computerArrows);
+  }
+
+  computerBase.display();
+  computer.display();
+  computer.life();
+  computerArcher.display();
+  handleComputerArrowCollision();
+}
+
+function keyPressed() {
+  if (keyCode === 32) {
+    var posX = playerArcher.body.position.x;
+    var posY = playerArcher.body.position.y;
+    var angle = playerArcher.body.angle;
+
+    var arrow = new PlayerArrow(posX, posY, 100, 10, angle);
+
+    arrow.trajectory = [];
+    Matter.Body.setAngle(arrow.body, angle);
+    playerArrows.push(arrow);
+  }
+}
+
+function keyReleased() {
+  if (keyCode === 32) {
+    if (playerArrows.length) {
+      var angle = playerArcher.body.angle;
+      playerArrows[playerArrows.length - 1].shoot(angle);
+    }
+    arrowSound.play();
+  }
+}
+
+function showArrows(index, arrows) {
+  arrows[index].display();
+  if(arrows[index].body.position.x>width||
+    arrows[index].body.position.y>height){
+      if(!arrows[index].isRemoved){
+        arrows[index].remove(index,arrows);
+      }else{
+        arrows[index].trajectory=[];
+      }
+
+    }
+
+}
+
+function handleComputerArcher() {
+  if (!computerArcher.collapse && !playerArcher.collapse) {
+    setTimeout(() => {
+      var pos = computerArcher.body.position;
+      var angle = computerArcher.body.angle;
+      var moves = ["UP", "DOWN"];
+      var move = random(moves);
+      var angleValue;
+
+      if (move === "UP"&&computerArcher.body.angle < 1.67) {
+        angleValue = 0.1;
+      } else {
+        angleValue = -0.1;
+      }
+      if(move === "DOWN" && computerArcher.body.angle > 1.47) {
+        angleValue = -0.1;
+      }else{
+          angleValue = 0.1;
+      }
+      angle += angleValue;
+
+      var arrow = new ComputerArrow(pos.x, pos.y, 100, 10, angle);
+
+      Matter.Body.setAngle(computerArcher.body, angle);
+      Matter.Body.setAngle(computerArcher.body, angle);
+
+      computerArrows.push(arrow);
+      setTimeout(() => {
+        computerArrows[computerArrows.length - 1].shoot(angle);
+      }, 100);
+
+      handleComputerArcher();
+    }, 2000);
+  }
+}
+
+function handlePlayerArrowCollision() {
+  for (var i = 0; i < playerArrows.length; i++) {
+    var baseCollision = Matter.SAT.collides(
+      playerArrows[i].body,
+      computerBase.body
+    );
+
+    var archerCollision = Matter.SAT.collides(
+      playerArrows[i].body,
+      computerArcher.body
+    );
+
+    var computerCollision = Matter.SAT.collides(
+      playerArrows[i].body,
+      computer.body
+    );
+
+    if (
+      baseCollision.collided ||
+      archerCollision.collided ||
+      computerCollision.collided
+    ) {
+     
+      computerArcherLife-= 1;
+      computer.reduceLife(computerArcherLife);
+      if(computerArcherLife<=0){
+        computerArcher.collapse = false;
+        Matter.Body.setStatic(computerArcher.body,false);
+        Matter.Body.setStatic(computer.body,false);
+        Matter.Body.setPosition(computer.body,{x:width-100,y:computer.body.position.y});
+       
+        gamewin();
+        
+        isGameWin = true;
+      }
+     
+    }
+    
+  }
+}
+
+function handleComputerArrowCollision() {
+  for (var i = 0; i < computerArrows.length; i++) {
+    var baseCollision = Matter.SAT.collides(
+      computerArrows[i].body,
+      playerBase.body
+    );
+
+    var archerCollision = Matter.SAT.collides(
+      computerArrows[i].body,
+      playerArcher.body
+    );
+
+    var playerCollision = Matter.SAT.collides(
+      computerArrows[i].body,
+      player.body
+    );
+
+    if (
+      baseCollision.collided ||
+      archerCollision.collided ||
+      playerCollision.collided
+    ) {
+      playerArcherLife-= 1;
+      player.reduceLife(playerArcherLife);
+      if(playerArcherLife <= 0){
+        playerArcher.collapse = true;
+        Matter.Body.setStatic(playerArcher.body,false);
+        Matter.Body.setStatic(player.body,false);
+        Matter.Body.setPosition(player.body,{x:100,y:player.body.position.y});
+        isGameOver = true;
+        gameover();
+
+      }
+
+    }
+  }
+}
+function gameover(){
+  swal(
+    {
+      title:`GameOver!!!`,
+      text:"Thanks for playing!",
+      imageUrl:"https://raw.githubusercontent.com/Hemachandrasai/pop/main/stickman-archer-game.jpg",
+      imageSize:"150x150",
+      confirmButtonText: "Play Again"
+
+    },
+    function (isConfirm) {
+      if (isConfirm) {
+        location.reload();
+      }
+    }
+  );
+}
+function gamewin(){
+  swal(
+    {
+      title:`You Win!!!`,
+      text:"Thanks for playing!",
+      imageUrl:"https://raw.githubusercontent.com/Hemachandrasai/win/main/pop.png",
+      imageSize:"150x150",
+      confirmButtonText: "Play Again"
+
+    },
+    function (isConfirm) {
+      if (isConfirm) {
+        location.reload();
+      }
+    }
+  );
+  
+}
+
